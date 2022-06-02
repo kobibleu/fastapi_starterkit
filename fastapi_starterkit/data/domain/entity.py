cc326658eab5813b3dd06772f080a9f6ff02d8af
kobibleu <vincent.terese@live.fr>
2022-06-02 16:47:42 +0100

    Add id to Entity

--- fastapi_starterkit/data/domain/entity.py
+++ fastapi_starterkit/data/domain/entity.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import declared_attr
 
 
 @as_declarative()
 class Entity:
+    id: Any
 
     @declared_attr
     def __tablename__(cls) -> str:
